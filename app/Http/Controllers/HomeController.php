223fe2dbbbe507dfa884b5c9f108231a002d3b75
nciganovic <nciganovic99@gmail.com>
2021-03-09 20:50:05 +0100

    Check if user is authenticated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,12 @@
 <?php
 namespace  App\Http\Controllers;
 
-class HomeController extends BaseController {
+use Illuminate\Support\Facades\Auth;
 
+class HomeController extends BaseController
+{
+    public function index(){
+        $this->data["auth"] = Auth::check();
+        return view('main.pages.index', $this->data);
+    }
 }
