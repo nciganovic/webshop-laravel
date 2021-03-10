4daf1490e3dcc1d11718734984a68477d80980c1
nciganovic <nciganovic99@gmail.com>
2021-03-10 14:30:32 +0100

    Get products specific to given category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,9 +4,16 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\Product;
+
 class CategoryController extends  BaseController
 {
-    public function Index(){
-        return view('main.pages.category');
+    public function Index($category_slug){
+
+        $this->data['products'] = Product::whereHas('categories', function ($q) use($category_slug){
+           $q->whereIn('slug', [$category_slug]);
+        })->get();
+
+        return view('main.pages.category', $this->data);
     }
 }
