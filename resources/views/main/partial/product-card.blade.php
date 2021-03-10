3ef4a23c307e140718c1d050382259cb229ff69a
nciganovic <nciganovic99@gmail.com>
2021-03-10 14:27:48 +0100

    Dynamicly add products

--- resources/views/main/partial/product-card.blade.php
+++ resources/views/main/partial/product-card.blade.php
@@ -1,19 +1,27 @@
-<div class="col-3 mt-3">
+<div class="col-4 mt-3">
     <a class="text-dark nounderline" href="#">
         <div class="card">
             <img
-                src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                src="{{ $product->image }}"
                 class="card-img-top"
-                alt="..."
+                alt="{{ $product->name }}"
             />
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">{{ $product->name }}</h5>
                 <p class="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the
-                    card's content.
+                    {{ $product->description }}
                 </p>
-                <div href="#!" class="btn btn-primary">10.99$</div>
-                <div href="#!" class="btn btn-primary">20%</div>
+                <div href="#!" class="btn btn-primary">
+                    @if($product->sale != 0)
+                        <s>{{ $product->price }}$</s>
+                    @else
+                        {{ $product->price }}$
+                    @endif
+                </div>
+                @if($product->sale != 0)
+                    <div class="btn btn-primary">{{ $product->price - (($product->price / 100) * $product->sale) }}$</div>
+                    <div class="btn btn-primary">20%</div>
+                @endif
             </div>
         </div>
     </a>
