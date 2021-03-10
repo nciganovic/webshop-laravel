07f419ce9f5bb7d7e4954f0beb774ff03255a4e9
nciganovic <nciganovic99@gmail.com>
2021-03-10 14:29:16 +0100

    Add is Auth check

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -1,13 +1,17 @@
 <?php
 namespace  App\Http\Controllers;
 
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+
 class BaseController extends Controller
 {
     public $data;
 
     public function __construct()
     {
-
+        $this->data['categories'] = Category::all();
+        $this->data['auth'] = Auth::check();
     }
 
 
