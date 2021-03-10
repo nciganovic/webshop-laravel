756e3e3b493e0bb126d2593cecad375af6c867c9
nciganovic <nciganovic99@gmail.com>
2021-03-10 14:30:07 +0100

    Display latest n products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,14 @@
 <?php
 namespace  App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends BaseController
 {
     public function index(){
-        $this->data["auth"] = Auth::check();
+        $this->data['products'] = Product::orderBy('created_at', 'DESC')->paginate(10);
+
         return view('main.pages.index', $this->data);
     }
 }
