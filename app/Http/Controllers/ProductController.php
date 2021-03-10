889dcc959f4a97a5f448e4d26d9bacb423b77d5d
nciganovic <nciganovic99@gmail.com>
2021-03-10 18:17:01 +0100

    Dynamicly show product in view

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,10 +4,15 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+
 class ProductController extends BaseController
 {
-    public  function index()
+    public  function index($product_slug)
     {
-        return view('main.pages.product');
+        $this->data['product'] = Product::with('categories')->with('sizes')->where('slug', '=', $product_slug)->first();
+        $this->data['auth'] = Auth::check();
+        return view('main.pages.product', $this->data);
     }
 }
