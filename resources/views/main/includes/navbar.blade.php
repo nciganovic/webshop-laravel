c74feafafc5b64bcd25de0dcb7954c188e340536
nciganovic <nciganovic99@gmail.com>
2021-03-10 14:28:11 +0100

    Dynamicly add categories on navbar

--- resources/views/main/includes/navbar.blade.php
+++ resources/views/main/includes/navbar.blade.php
@@ -1,20 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar w/ text</a>
+        <a class="navbar-brand" href="/">Shop</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                @foreach($categories as $ctg)
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('category', ['category_slug' => $ctg->slug]) }}">{{ $ctg->name }}</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
+                @endforeach
             </ul>
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 @if($auth)
