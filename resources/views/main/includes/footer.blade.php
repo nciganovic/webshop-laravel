8ce25d9e1e8db60acf251c13be1cae20c230ae7d
nciganovic <nciganovic99@gmail.com>
2021-03-13 20:57:29 +0100

    Remove padding bottom

--- resources/views/main/includes/footer.blade.php
+++ resources/views/main/includes/footer.blade.php
@@ -17,7 +17,7 @@
         </div>
     </div>
     <div class="container-fluid">
-        <div class="row pb-5">
+        <div class="row">
             <div class="col-12 d-flex justify-content-center">
                 <p class="text-center mt-3">© 2020 Copyright: Nikola Ciganovic. All rights reserved.</p>
             </div>
