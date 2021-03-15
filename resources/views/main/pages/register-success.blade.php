f0869abd135e8f4353ed7359e5d5582fc2fde716
nciganovic <nciganovic99@gmail.com>
2021-03-15 15:51:47 +0100

    text center

--- resources/views/main/pages/register-success.blade.php
+++ resources/views/main/pages/register-success.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-12">
             <h1 class="text-center">Successful registration</h1>
-            <p>Go to <a href="{{ route('login_get') }}">login page</a> to start using app.</p>
+            <p class="text-center">Go to <a href="{{ route('login_get') }}">login page</a> to start using app.</p>
         </div>
     </div>
 @endsection
