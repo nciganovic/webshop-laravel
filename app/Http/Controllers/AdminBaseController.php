d2f859c2f6fb793ca3c8c52ccf8f33ea7779d4d2
nciganovic <nciganovic99@gmail.com>
2021-03-15 15:41:00 +0100

    Remove middleware in constructor

--- app/Http/Controllers/AdminBaseController.php
+++ app/Http/Controllers/AdminBaseController.php
@@ -12,22 +12,12 @@ class AdminBaseController extends Controller
 {
     protected $data = [];
 
-    public function __constructor(){
-        $this->middleware(function ($request, $next) {
-            $this->data['user'] = $this->GetAdmin();
-            if($this->data['user'] == null){
-                return redirect()->route('home');
-            }
-            return $next($request);
-        });
-    }
-
     public function GetAdmin()
     {
         if(Auth::check()){
             $user = User::where('id', '=', Auth::id())->first();
             if($user != null){
-                $role = Role::where('id', '=', $user->id)->first();
+                $role = Role::where('id', '=', $user->role_id)->first();
                 if($role->name == "admin"){
                     return $user;
                 }
